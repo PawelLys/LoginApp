991336823863c5372afbf03971e251411b462e8b
PawelLys <53062631+PawelLys@users.noreply.github.com>
2019-07-25 15:50:33 +0200

    Updates

---
+++ precache-manifest.fd2a12c6752e4fa6ba6e7911a12ff9a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32819ed881c41cb4840801b93bc32bf0",
+    "url": "/LoginApp/index.html"
+  },
+  {
+    "revision": "c4dc858077ca810e9cfb",
+    "url": "/LoginApp/static/js/2.90491335.chunk.js"
+  },
+  {
+    "revision": "83d4550b4cfdcf0a95cc",
+    "url": "/LoginApp/static/js/main.5f9dba05.chunk.js"
+  },
+  {
+    "revision": "0dd462a91aee54694c31",
+    "url": "/LoginApp/static/js/runtime~main.85bf3aea.js"
+  }
+]);
